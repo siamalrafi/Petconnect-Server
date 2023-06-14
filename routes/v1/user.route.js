4f73b1491050504682b2489aecb0bbd24fbe340b
siamalrafi <rahathasandipto@gmail.com>
2023-06-14 11:58:19 +0600

    enw

--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -4,7 +4,6 @@ const verifyToken = require("../../middleware/verifyToken");
 const router = express.Router();
 
 router.post("/signup", userController.signup);
-// router.get("/signup/confirmation/:token", userController.confirmEmail);
 
 router.post("/login", userController.login);
 
